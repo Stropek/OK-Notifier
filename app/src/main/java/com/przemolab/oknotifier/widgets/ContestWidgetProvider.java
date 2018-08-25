8a0434ed28c24917ed0b1ae01b7c9d1dc4b73763
przemo <curzytekp@gmail.com>
2018-08-25 15:39:29 -0400

    fix build issues

--- app/src/main/java/com/przemolab/oknotifier/widgets/ContestWidgetProvider.java
+++ app/src/main/java/com/przemolab/oknotifier/widgets/ContestWidgetProvider.java
@@ -15,7 +15,6 @@ public class ContestWidgetProvider extends AppWidgetProvider {
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.contest_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
